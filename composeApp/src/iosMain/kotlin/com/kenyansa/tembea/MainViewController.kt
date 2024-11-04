ec7f57e49f747eb20a9d200249bc2494383ff6c1
Felix Amenya <kenyansafelix@gmail.com>
2024-11-04 23:18:07 +0300

    server side

---
+++ composeApp/src/iosMain/kotlin/com/kenyansa/tembea/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.kenyansa.tembea
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/com/kenyansa/tembea/Constants.kt
@@ -0,0 +1,3 @@
+package com.kenyansa.tembea
+
+const val SERVER_PORT = 8080
